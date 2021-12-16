e78ce3c981f4c868a1b975dc1f8ebab6810dda13
guest1 <akm17122003@gmail.com>
2021-12-16 21:17:55 +0600

    commit

--- app/src/main/java/kg/kamila/individ_project/IntroActivity.kt
+++ app/src/main/java/kg/kamila/individ_project/IntroActivity.kt
@@ -3,9 +3,9 @@ package kg.kamila.individ_project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
     }
 }
