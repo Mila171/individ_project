b90d81d5cd5feef52e805e2ce090fbe2e94e8261
guest1 <akm17122003@gmail.com>
2021-12-21 15:09:05 +0600

    main page

--- app/src/main/java/kg/kamila/individ_project/HomeActivity.kt
+++ app/src/main/java/kg/kamila/individ_project/HomeActivity.kt
@@ -1,11 +1,32 @@
 package kg.kamila.individ_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kg.kamila.individ_project.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val nav = findViewById<BottomNavigationView>(R.id.nav)
+
+            nav.setOnNavigationItemReselectedListener {
+                when (it.itemId) {
+                    R.id.home -> {
+                        startActivity(Intent(this@HomeActivity, HomeActivity::class.java))
+                    }
+                    R.id.discount -> {
+                        startActivity(Intent(this@HomeActivity, Intro2Activity::class.java))
+                    }
+
+
+            }
+        }
+
+
     }
 }
